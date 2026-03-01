00b84844fa03a95d640c8f8215899d2595ba3574
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-01 02:58:08 -0300

    feat(auth): enhance AuthScreen layout and functionality
    - Import necessary components for improved UI
    - Refactor layout for better structure and readability
    - Add navigation functionality for account creation and login
    - Include local data indication with an icon

--- src/feature/auth/page.tsx
+++ src/feature/auth/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { HatGlasses } from "lucide-react-native";
+
 import { Text } from "@/components/ui/text";
+import { Button } from "@/components/ui/button";
+import { Icon } from "@/components/ui/icon";
+import { useNavigation } from "@react-navigation/native";
 
 import Illustration1 from "../../../assets/images/pexels-snapwire-7470.jpg";
-import { Button } from "@/components/ui/button";
 
 export const AuthScreen = () => {
+  const navigation = useNavigation();
   return (
-    <SafeAreaView className="flex-1 flex-col justify-between bg-background items-center gap-6 py-6 px-6">
-      <View className="h-full max-h-[400px] w-full flex-1 flex flex-col items-center justify-center">
+    <SafeAreaView className="w-full h-full flex flex-col justify-between bg-background items-center gap-6 py-4">
+      <View className="max-h-[400px] h-[400px] w-full flex-1 flex flex-col items-center justify-center px-6">
         <Image
           source={Illustration1}
           resizeMode="contain"
@@ -24,11 +29,37 @@ export const AuthScreen = () => {
         </Text>
       </View>
 
-      <Button variant="default" size="lg" className="w-full">
-        <Text className="text-primary-foreground font-semibold text-base">
-          Crie sua conta
+      <View className="w-full max-h-10 mt-auto items-center justify-center gap-2 flex flex-row">
+        <View className="w-full h-px bg-muted flex-1" />
+        <Icon as={HatGlasses} size={24} className="text-muted" />
+        <Text variant="default" className="text-center text-muted">
+          Dados 100% local
         </Text>
-      </Button>
+        <View className="w-full h-px bg-muted flex-1" />
+      </View>
+      <View className="w-full h-max items-center gap-6 p-6 flex flex-col">
+        <Button
+          variant="default"
+          size="lg"
+          className="w-full"
+          onPress={() => navigation.navigate("CreateAccount")}
+        >
+          <Text className="text-primary-foreground font-semibold text-base">
+            Crie sua conta
+          </Text>
+        </Button>
+
+        <Button
+          variant="outline"
+          size="lg"
+          className="w-full"
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text className="text-primary font-semibold text-base">
+            Já tenho uma conta
+          </Text>
+        </Button>
+      </View>
     </SafeAreaView>
   );
 };
