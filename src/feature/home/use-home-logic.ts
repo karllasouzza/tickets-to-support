0faeb84c17b9ac0e59329b3255f4ad11498a54c8
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-01 13:40:41 -0300

    refactor(home): improve ticket sorting logic - Sort tickets by creation date when filter is "all" - Maintain sorting for filtered tickets based on status

--- src/feature/home/use-home-logic.ts
+++ src/feature/home/use-home-logic.ts
@@ -8,8 +8,16 @@ export default function useHomeLogic() {
 
   const filteredTickets =
     selectedFilter === "all"
-      ? tickets
-      : tickets.filter((t) => t.status === (selectedFilter as TicketStatus));
+      ? tickets.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        )
+      : tickets
+          .filter((t) => t.status === (selectedFilter as TicketStatus))
+          .sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+          );
 
   return { tickets, filteredTickets, selectedFilter, setSelectedFilter };
 }
