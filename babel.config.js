a009948b05f8f41e615d74920f34b9bd06b3433f
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-28 23:07:47 -0300

    chore: clean up empty code change entries
    - Removed multiple empty <code-changes> entries
    - Improved readability of change log

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset", "nativewind/babel"],
+  plugins: [
+    "react-native-worklets/plugin",
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@": "./src",
+        },
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,14 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+import { getDefaultConfig, mergeConfig } from "@react-native/metro-config";
+import { withNativeWind } from "nativewind/dist/metro/index.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = mergeConfig(getDefaultConfig(__dirname), {});
+
+export default withNativeWind(config, {
+  input: "./src/styles/global.css",
+  inlineRem: 16,
+});
